efd10190cd87a3c93e5d9c97c7207b0ddb25fa99
Heimdall Rojas <soporte@integratic.com.co>
2023-04-10 21:45:00 -0500

    Ciclo for

--- do_while.php
+++ do_while.php
@@ -1,12 +1,12 @@
 <?php
 
-$usernames = []
+$usernames = [];
 
 do {
      echo "Esto se ejecuta una vez";
 } while ($a <= 10) {
     # code...
-}
+};
 
 
 
---
+++ for.php
@@ -0,0 +1,8 @@
+<?php
+for ($contador=1; $contador <=10 ; $contador++) { 
+    # code...
+    echo $contador . "\n";
+}
+echo "\n";
+
+?>
